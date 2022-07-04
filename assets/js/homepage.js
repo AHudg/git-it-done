2101cdc5184752476441cc0baf2806403374d1e2
Andrew Hudgins <andrewhudgins15@gmail.com>
2022-07-04 18:51:51 -0500

    Completed module 4

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -41,7 +41,7 @@ var formSubmitHandler = function(event) {
 
 var displayRepos = function(repos, searchTerm) {
     // check if api returned any repos
-    if (repo.length === 0) {
+    if (repos.length === 0) {
         repoContainerEl.textContent = "No repositories found.";
         return;
     }
@@ -55,8 +55,9 @@ var displayRepos = function(repos, searchTerm) {
         var repoName = repos[i].owner.login + "/" + repos[i].name;
 
         // create a container for each repo
-        var repoEl = document.createElement("div");
+        var repoEl = document.createElement("a");
         repoEl.classList = "list-item flex-row justify-space-between align-center";
+        repoEl.setAttribute('href','./single-repo.html?repo=' + repoName);
 
         // create a span element to hold the repository name
         var titleEl = document.createElement("span");
